9756f0d648146760bbb10c0d09284934cc4617b0
Prem Roshan Nair <premroshan@gmail.com>
2020-08-08 18:37:20 +0530

    lists

---
+++ chapter5/banned_users.py
@@ -0,0 +1,5 @@
+banned_users = ['sagu','goobi','kiluki']
+user = 'ani'
+
+if user not in banned_users:
+    print(f"{user.title()}, you can post a response")
---
+++ chapter5/voting.py
@@ -0,0 +1,13 @@
+# voting app
+age = 19
+if age>=18:
+    print("You are old enough to vote!")
+
+age = 12
+if age < 4:
+    print("Your admission cost is $0")
+elif age < 18:
+    print("Your admission cost is $25")
+else:
+    print("Your admission cost is 40$")
+    
