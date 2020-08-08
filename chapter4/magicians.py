eea17ad109e2ad2fc297e49359565cffb0585ce2
Prem Roshan Nair <premroshan@gmail.com>
2020-08-08 08:28:09 +0530

    Update magicians.py

--- chapter4/magicians.py
+++ chapter4/magicians.py
@@ -2,4 +2,7 @@
 magicians = ['alice','david','carolina']
 for magician in magicians:
 	print(f"{magician.title()}, that was a great trick")
-	print()
+	print(f"I would like to watch your next show {magician.title()}")
+
+for value in range(1,10):
+	print(value)
